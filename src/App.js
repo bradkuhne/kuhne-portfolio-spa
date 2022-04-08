45be2dcd22e2d9d8503f803867c847c688087fdd
Brad Kuhne <bjkuhne@aol.com>
2022-04-08 15:28:57 -0500

    skeleton nav bar setup with categories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
+
+  const [categories] = useState([
+    {name: 'About Me'},
+    { name: 'Portfolio', description: 'Select projects developed by Brad Kuhne...' },
+    { name: 'Resume', description: 'Downloadable resume for Brad Kuhne...' },
+    {name: 'Contact', description: 'Let me know if you have any questions or would like me to reach out to you.'}
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
+      <Nav
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
+      >
+      </Nav>
       <main>
         <About></About>
       </main>
---
+++ src/components/Nav/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
+
+function Nav(props) {
+
+    const {
+        categories = [],
+        setCurrentCategory,
+        contactSelected,
+        currentCategory,
+        setContactSelected,
+    } = props;
+
+    return (
+        <header className="flex-row px-1">
+            <h2>
+                <a href="/">
+                    Brad Kuhne's Portfolio
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    {categories.map((category) => (
+                        <li
+                            className={`mx-1 ${currentCategory.name === category.name && !contactSelected && 'navActive'
+                                }`}
+                            key={category.name}
+                        >
+                            <span
+                                onClick={() => {
+                                    setCurrentCategory(category);
+                                    setContactSelected(false);
+                                }}
+                            >
+                                {capitalizeFirstLetter(category.name)}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+
+            </nav>
+        </header>
+    );
+}
+
+export default Nav;
