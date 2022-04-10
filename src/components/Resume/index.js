ca5bc42a0460f0d8a08331f188b2b21319cc6b83
Brad Kuhne <bjkuhne@aol.com>
2022-04-09 23:01:20 -0500

    formatted resume experience list and download placeholder

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,6 +5,27 @@ const Resume = () => {
     return (
         <section className="my-5">
             <h1>Proficiencies and Resume</h1>
+            <h2 className="my-5"> Recently Acquired Skills</h2>
+            <ul>
+                <li>React</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>MongoDB</li>
+                <li>NodeJS</li>
+                <li>ExpressJS</li>
+                <li>Sequelize</li>
+                <li>Restful APIs</li>
+                <li>Git</li>
+            </ul>
+            <h2 className="my-5">Prior Career Skills</h2>
+            <ul>
+                <li>Project Management (20 yrs)</li>
+                <li>Scrum Master (4 years)</li>
+                <li>Client Relationship Management (15 years)</li>
+                <li>Business Analysis (20 years)</li>
+            </ul>
+            <h2 className="my-5">Downloadable Resume</h2>
         </section>
     )
 }
