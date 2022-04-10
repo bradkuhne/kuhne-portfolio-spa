05b965f91ea542fd413c70aa316b2a70314db038
Brad Kuhne <bjkuhne@aol.com>
2022-04-09 22:06:45 -0500

    skeleton resume page

---
+++ src/components/Resume/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Resume = () => {
+
+    return (
+        <section className="my-5">
+            <h1>Proficiencies and Resume</h1>
+        </section>
+    )
+}
+export default Resume;
